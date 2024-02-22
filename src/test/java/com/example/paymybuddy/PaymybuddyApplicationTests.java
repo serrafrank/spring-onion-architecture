c8bfa919595eda5fb439e76a826d5292d8387734
SERRA Frank <frank.serra@keymaging.com>
2024-02-22 15:10:10 +0100

    init project

---
+++ src/test/java/com/example/paymybuddy/PaymybuddyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.paymybuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class PaymybuddyApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+
+}
